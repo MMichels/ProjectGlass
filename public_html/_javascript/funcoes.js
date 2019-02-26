998c7296efb911ad43961d60338132cfc17de15d
Mateus <Mateus@Pc-Mateus>
2019-02-25 23:00:00 -0300

    Aula 33 - Finalizando o formulario com css3 e javascript!:  - concluida a formatação visual do formulario;  - adicionado uma pequena function em javascript para o calculo automatico dos preços; - curso finalizado!.

--- public_html/_javascript/funcoes.js
+++ public_html/_javascript/funcoes.js
@@ -1,3 +1,9 @@
 function mudaFoto(foto){
     document.getElementById("img_oculos_pequeno").src = foto;
 }
+
+function calc_total(){
+    var qtd = parseInt(document.getElementById('spn_qntd').value);
+    tot = qtd * 1500;
+    document.getElementById('txt_total').value = tot;
+}
